040dcb7dcaff8fd888c033b208b58e0c017249df
gagelafr <gagelafr@gmail.com>
2024-02-18 15:21:48 -0700

    Caption Change

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -21,7 +21,7 @@ class AlienIvasion:
         else:
             self.screen = pygame.display.set_mode(
                 (self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion!")
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
