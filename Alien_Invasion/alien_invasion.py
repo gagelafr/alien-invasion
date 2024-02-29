150751896dd500564013b2e8a060598cda4790bd
gagelafr <gagelafr@gmail.com>
2024-02-29 06:25:28 -0700

    Aliens_can_be_shot

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -83,6 +83,11 @@ class AlienIvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        # Chcek for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
     
     def update_aliens(self):
         """
